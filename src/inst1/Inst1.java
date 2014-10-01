e9502ac39716da0a076b5fe7ffdd545c93dc9afc
Vincent <bd391105@C39481.ens.uqam.ca>
2014-10-01 10:21:31 -0400

    Commit de la inst2

--- src/inst1/Inst1.java
+++ src/inst1/Inst1.java
@@ -17,7 +17,7 @@ public class Inst1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("allo");
+        System.out.println("hey");
     }
     
 }
